96090d27887fa2dc77e00e00b63cfcb8e3eeb59f
Mr.Fani <fani@xino.digital>
2024-12-01 16:56:25 +0100

    update v3

--- js/options_main_page.js
+++ js/options_main_page.js
@@ -1,15 +1,21 @@
-var panel = JSON.parse(localStorage.getItem("option_panel"));
-var arguments = getUrlVars();
-var element;
+import '/lib/jquery-3.3.1.min.js';
+import { getUrlVars } from '/js/utils.js';
+import { storage } from '/js/common-utils.js';
 
-if (panel === "null" || panel === null || panel === undefined) {
-    element = "support";
-} else {
+async function start() {
+  const panel = await storage.get('option_panel')
+  const urlVars = getUrlVars();
+  let element;
+  if (panel === 'null' || panel === null || panel === undefined) {
+    element = 'support';
+  } else {
     element = panel;
-}
+  }
 
-if (arguments.page !== undefined) {
-    element = arguments.page;
-}
+  if (urlVars.page !== undefined) {
+    element = urlVars.page;
+  }
 
-location.href = "/options_pages/" + element + ".html";
+  location.href = '/options_pages/' + element + '.html';
+}
+window.addEventListener('load', start);
